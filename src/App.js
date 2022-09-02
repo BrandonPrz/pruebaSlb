16af3f966d38ebb258aa180b4924c235ac2f6abd
BrandonPRZ <a217207765@unison.mx>
2022-09-02 16:13:17 -0700

    hice-link-funcionar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <Link to="/">Home</Link>
+    <br />
+    <Link to="/category">Category</Link>
+    <br />
+    <Link to="/products">Products</Link>
+  </div>
+);
+
+const Category = () => (
+  <div>
+    <h2>Category</h2>
+  </div>
+);
+
+const Products = () => (
+  <div>
+    <h2>Products</h2>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          jUan <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/category">
+          <Category />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+      </Switch>
     </div>
   );
 }
-
 export default App;
